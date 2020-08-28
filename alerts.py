30855cc8b34db79f81e24e23a3619f96701f8431
genilson <genilsonisrael@gmail.com>
2020-08-28 13:01:23 -0300

    Some more messages printed to stdout. Informing the server in advance how many packages it should wait

--- alerts.py
+++ alerts.py
@@ -22,12 +22,12 @@ client_server_group.add_argument('-a', '--alert-port', dest='alert_port', metava
 
 server_group = parser.add_argument_group('Server specific')
 # Timeout 
-server_group.add_argument('-t', '--time-out', type=float, dest='timeout', metavar='#', default=5.0, help='Timeout in seconds (default 5s)')
+server_group.add_argument('-t', '--time-out', type=float, dest='timeout', metavar='#', default=5.0, help='Timeout in seconds after last packet received (default 5s)')
 
 client_group = parser.add_argument_group('Client specific')
 client_group.add_argument('-n', '--num-packets', type=int, dest='num_pkts', metavar='#', default=50, help='number of packets to send (default 50)')
 # Inter-packet gap in seconds. Default is 10 milliseconds
-client_group.add_argument('-i', '--inter', type=float, dest='inter_pkts', metavar='#', default=0.01, help='Interpacket gap (default 10ms)')
+client_group.add_argument('-i', '--inter', type=float, dest='inter_pkts', metavar='#', default=0.01, help='Inter-packet gap in seconds (default 0,01s)')
 
 args = parser.parse_args();
 
@@ -55,14 +55,19 @@ if args.server:
     try:
         # Running the server
         while True:
+
+            # Getting parameters from client
+            con,client = tcp.accept()
+            num_pkts_client = int.from_bytes(con.recv(100),'little')
+            con.close()
+
             last_pkt_time = 0.0
 
             # Setting asynchronous sniffer
-            result = AsyncSniffer(prn=time_last_packet,filter='udp port {}'.format(args.alert_port), count=args.num_pkts)
-
+            result = AsyncSniffer(prn=time_last_packet,filter='udp port {}'.format(args.alert_port), count=num_pkts_client)
             result.start()
 
-            print('Sniffing packets on port {}'.format(args.alert_port))
+            print('Sniffing {} packets from {} on port {}'.format(num_pkts_client, client[0], args.alert_port))
 
             # The loop only leaves when num_pkts packets were received, or
             # timeout after last received packet is reached                                                       
@@ -71,7 +76,7 @@ if args.server:
                     if (time.time() - last_pkt_time) >= args.timeout:
                         result.stop()
 
-            # Giving the sniffer the time it needs to stop
+            # Giving the sniffer time to start
             time.sleep(0.5)
 
             # Dictionary with info about sniffed packets           
@@ -80,6 +85,7 @@ if args.server:
             # Getting info from the client about packets that were sent
             con, client = tcp.accept()
             print('Connection from {} established'.format(client))
+            print('Getting information on sent packages from client')
             chunks = []
             while True:
                 msg = con.recv(1024)
@@ -88,7 +94,6 @@ if args.server:
                 else:
                     chunks.append(msg)
             print('Closing connection with {}'.format(client))
-            #print('Got {} bytes from {}'.format(sys.getsizeof(chunks),client))
             con.close()
 
             # Decoding the message into a dictionary
@@ -99,6 +104,8 @@ if args.server:
             loss = 0
             log = open(time.strftime('%d_%m_%Y_%H_%M_%S')+'_delays.csv', 'w')
             log.write('id,delay\n')
+
+            print('Calculating and logging packets delays and packet loss')
 
             # Recv_dict has info about app packets that were sent, regardless if they
             # were received or not
@@ -119,6 +126,19 @@ if args.server:
 
 # Client mode
 else:
+    HOST = args.host
+    PORT = args.port
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dest = (HOST, PORT)
+
+    # Sending parameters info
+    tcp.connect(dest)
+    tcp.sendall((args.num_pkts).to_bytes(((args.num_pkts).bit_length() + 7) // 8, 'little'))
+    #tcp.close()
+
+    # Giving the sniffer time to start
+    time.sleep(0.5)
+
     # Sending packets and storing their ids and sent timestamps
     # TODO: Set correct TOS and DSCP fields
     pkts_sent = send(IP(dst=args.host, id=range(1,args.num_pkts+1),tos=192)/UDP(dport=args.alert_port),
@@ -130,13 +150,9 @@ else:
     # Sending infos to the server
     encoded_msg = json.dumps(infos).encode('utf-8')
 
-    HOST = args.host
-    PORT = args.port
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    dest = (HOST, PORT)
     tcp.connect(dest)
     tcp.sendall(encoded_msg)
-    #print('Sent {} bytes to {}'.format(sys.getsizeof(bytes(encoded_msg)), args.host))
     tcp.close()
     
 # if args.server and 'num_pkts' in vars(args):
